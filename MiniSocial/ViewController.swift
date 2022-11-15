d26a976fc4448ec678435f47343c6c82d52466c4
Eric Truong <thaoth58@gmail.com>
2022-11-15 19:54:26 +0900

    Initial Commit

---
+++ MiniSocial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniSocial
+//
+//  Created by Thao Truong on 15/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
